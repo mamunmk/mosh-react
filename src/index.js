908b1161a1e2e6d5979a6499d18144fb1aa96a28
mamunmk <mamun.k2m@gmail.com>
2022-02-14 02:40:47 +0600

    JavaScript data type and object

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function AApp() {
+  const name = "Mamun";
+  const fullName = "Arafat Nahian";
+  const age = 25;
+  let subscriber = 1500;
+  const html = `<div>React is funny!</div>`;
+
+  const obj = {
+    fullName: "Saif Uddin",
+    age: 30,
+    [fullName]: fullName,
+  };
+
+  return (
+    <div className="container">
+      {html}
+      {name}
+      <a>Hello, {name}!</a>
+      <a>Subscriber, {subscriber}</a>
+      <a>Hello, {obj.fullName}!</a>
+      <a>Hello, {obj[fullName]}!</a>
+    </div>
+  );
+}
+ReactDOM.render(<AApp />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
